674ccac5ca817590ad77b5a7f0252f15a2ed2dd2
buenahora <buenahorafranco@gmail.com>
2022-10-17 09:33:09 -0300

    script

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-alert("algo");
+var totalPuntaje = 100;
+
+// Situacion 1
+// Prometiste algo pero ahora se te dificulta cumplirlo, que harias?
+// Romper la promesa
+// Hacer todo lo que puedas por cumplirla
+// ??
+
+// Situacion 2
+// Ves a alguien que precisa ayuda, que harias?
+// Ayudarlo
+// Dejarlo
+
+// Situacion 3
+// 
+// 
+// 
+
+// Situacion 4
+// 
+// 
+// 
+
+// Situacion 5
+// 
+// 
+// 
+
+// Situacion 6
+// 
+// 
+// 
+
+// Situacion 7
+// 
+// 
+// 
+
+// Situacion 8
+// 
+// 
+// 
+
+// Situacion 9
+// 
+// 
+// 
+
+// Situacion 10
+// 
+// 
+// 
+
