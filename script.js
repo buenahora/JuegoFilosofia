ea68486c682912461c17526f7eaf9f454ae16bdb
buenahora <buenahorafranco@gmail.com>
2022-10-19 17:50:21 -0300

    anda todo

--- script.js
+++ script.js
@@ -2,6 +2,61 @@ var totalPuntaje = 0;
 var widthProgressBar = 0;
 var barraProgreso = document.querySelector(".progress-bar");
 
+
+function randomize(collection) {
+    var randomNumber = Math.floor(Math.random() * collection.length);
+    return collection[randomNumber];
+  }
+  
+  function confetti() {
+    $(".confetti").remove();
+    var $confettiItems = $('<div class="confetti"></div>'),
+      colors = ["#3b5692", "#f9c70b", "#00abed", "#ea6747"],
+      height = 6.6,
+      width = 6.6;
+  
+    var scale, $confettiItem;
+  
+    for (var i = 0; i < 100; i++) {
+      scale = Math.random() * 1.75 + 1;
+      $confettiItem = $(
+        "<svg class='confetti-item' width='" +
+          width * scale +
+          "' height='" +
+          height * scale +
+          "' viewbox='0 0 " +
+          width +
+          " " +
+          height +
+          "'>\n  <use transform='rotate(" +
+          Math.random() * 360 +
+          ", " +
+          width / 2 +
+          ", " +
+          height / 2 +
+          ")' xlink:href='#svg-confetti' />\n</svg>"
+      );
+      $confettiItem.css({
+        animation:
+          Math.random() +
+          2 +
+          "s " +
+          Math.random() * 2 +
+          "s confetti-fall ease-in both",
+        color: randomize(colors),
+        left: Math.random() * 100 + "vw"
+      });
+      $confettiItems.append($confettiItem);
+    }
+    $("body").append($confettiItems);
+  }
+  
+  $("#congratsModal").on("shown.bs.modal", function() {
+    confetti();
+  });
+  
+
+
 let opciones = [
     {
         pregunta: "Prometiste algo pero ahora se te dificulta cumplirlo, que harias?",
@@ -143,6 +198,29 @@ opciones.map( (opcion, index) => {
                 if(esPenultimo) {
                     situacionProxima.querySelector("#confirmar").textContent = "Ver resultado";
                     situacionProxima.querySelector("#confirmar").disabled = false;
+
+                    situacionProxima.querySelector("#confirmar").addEventListener("click", () => {
+                        let modal = document.querySelector('#congratsModal');
+                        modal.classList.remove("fade")
+                        modal.style.display = "block"
+                        modal.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+
+                        document.querySelector("#txtResultado").textContent = `${totalPuntaje}%`
+                    
+                        confetti()
+                    
+                        modal.addEventListener("click", () => {
+                            modal.remove()
+                        })
+                        // let modalDialog = document.querySelector('#modalDialog');
+                        // modalDialog.style.display = "block"
+                        // modalDialog.style.opacity = "1";
+                    })
+
+
+                    
+                
+                
                 }
                 
     
